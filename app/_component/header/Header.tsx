760f44917d0d9c65a796b1b7b4e4bc23291e9c1f
Om Bharti <ombharti888@gmail.com>
2024-01-26 20:47:15 +0530

    center heading

--- app/_component/header/Header.tsx
+++ app/_component/header/Header.tsx
@@ -1,5 +1,26 @@
+import Image from "next/image";
 import "./index.css";
 
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="logo-box">
+        <Image
+          className="logo"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub">Is where life happens</span>
+        </h1>
+      </div>
+      Header
+    </header>
+  );
 };
