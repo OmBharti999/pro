7481141708d32e857bf4f1ad96e7eae2a7e33f8b
Om Bharti <ombharti888@gmail.com>
2024-01-27 20:36:27 +0530

    fix update ui

--- app/_component/header/Header.tsx
+++ app/_component/header/Header.tsx
@@ -1,26 +1,33 @@
 import Image from "next/image";
+import { LinkButton } from "@/app/_component";
+
 import "./index.css";
 
 export const Header = () => {
   return (
     <header>
       <div className="logo-box">
-        <Image
+        {/* <Image
           className="logo"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
+        /> */}
+        <h2>Logo</h2>
       </div>
       <div className="text-box">
         <h1 className="heading-primary">
           <span className="heading-primary-main">Outdoors</span>
           <span className="heading-primary-sub">Is where life happens</span>
         </h1>
+        <LinkButton
+          addClass="btn-white"
+          url={"#"}
+          btnName="Discover Our tour"
+        />
       </div>
-      Header
     </header>
   );
 };
