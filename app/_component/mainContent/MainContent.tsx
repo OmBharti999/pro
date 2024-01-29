838888045351a9923915c8123922f54d6546ab01
Om Bharti <ombharti888@gmail.com>
2024-01-29 23:41:51 +0530

    styling heading secondary

--- app/_component/mainContent/MainContent.tsx
+++ app/_component/mainContent/MainContent.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+import "./index.css";
+
 export const MainContent = () => {
-  return <main>MainContent</main>;
+  return (
+    <main>
+      <section className="section-about">
+        <h2 className="heading-secondary">
+          Existing tour for adventure people
+        </h2>
+      </section>
+    </main>
+  );
 };
