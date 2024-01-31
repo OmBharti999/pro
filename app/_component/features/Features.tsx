4d8e5c0e6f406ef4c7d1e31467a3d8a5dc7a6a71
Om Bharti <ombharti888@gmail.com>
2024-02-01 00:59:14 +0530

    screw effect

--- app/_component/features/Features.tsx
+++ app/_component/features/Features.tsx
@@ -5,11 +5,13 @@ import "./index.css";
 export const Features = () => {
   return (
     <>
-      <section className="section-feature flex-wrap">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <section className="section-feature flex-wrap -skew-y-6">
+        <div className="cards-container skew-y-6">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </section>
     </>
   );
