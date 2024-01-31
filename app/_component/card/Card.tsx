2633de43d8377b96149669cdd5773e68f1b43270
Om Bharti <ombharti888@gmail.com>
2024-01-31 23:44:32 +0530

    removing svg files

--- app/_component/card/Card.tsx
+++ app/_component/card/Card.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+// import Image from "next/image";
+import React from "react";
 
 export const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div>
+      {/* <Image src={Globe} alt="globe-icon" width={100} height={100} /> */}
+      {/* <Globe /> */}
+    </div>
+  );
+};
--- app/_component/svg/index.ts
+++
@@ -1,4 +0,0 @@
-export * as Heart from "./heart.svg";
-export * as Globe from "./globe.svg";
-export * as Campus from "./campus.svg";
-export * as Map from "./map.svg";
