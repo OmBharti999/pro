5d2e17285899791e46908540004f61cfc97e3717
Om Bharti <ombharti888@gmail.com>
2024-02-01 00:03:03 +0530

    section bg same effect add

--- app/_component/features/Features.tsx
+++ app/_component/features/Features.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Card } from "..";
+import "./index.css"
 
 export const Features = () => {
   return (
     <>
       <section className="section-feature">
+        fdsf
+        <Card />
+        <Card />
         <Card />
         <Card />
       </section>
