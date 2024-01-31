04ea49365521c5f582ae303b92ac5026c285a14b
Om Bharti <ombharti888@gmail.com>
2024-02-01 00:02:50 +0530

    add card css

--- app/_component/card/Card.tsx
+++ app/_component/card/Card.tsx
@@ -1,11 +1,15 @@
 // import Image from "next/image";
 import React from "react";
 
+import "./index.css"
+
+
 export const Card = () => {
   return (
-    <div>
+    <div className="card w-64 bg-gray-100 h-72">
       {/* <Image src={Globe} alt="globe-icon" width={100} height={100} /> */}
       {/* <Globe /> */}
+
     </div>
   );
 };
