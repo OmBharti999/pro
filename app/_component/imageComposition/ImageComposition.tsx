9fee0afaa487f762f1ceba2c5e403cdeb61d8153
Om Bharti <ombharti888@gmail.com>
2024-01-31 22:57:33 +0530

    update image position

--- app/_component/imageComposition/ImageComposition.tsx
+++ app/_component/imageComposition/ImageComposition.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import "./index.css";
 
 const commonWidth = 240;
-const commonHeight = 180;
+const commonHeight = 190;
 
 export const ImageComposition = () => {
   return (
@@ -14,21 +14,21 @@ export const ImageComposition = () => {
         alt=""
         width={commonWidth}
         height={commonHeight}
-        className="left-0 top-0 img-big-effect"
+        className="left-0 top-2 img-big-effect"
       />
       <Image
         src={"/assets/image-2.jpg"}
         alt=""
         width={commonWidth}
         height={commonHeight}
-        className="left-72 top-20 img-big-effect"
+        className="left-52 top-20 img-big-effect"
       />
       <Image
         src={"/assets/image-3.jpg"}
         alt=""
         width={commonWidth}
         height={commonHeight}
-        className="left-24 top-48 img-big-effect"
+        className="left-20 top-40 img-big-effect"
       />
     </div>
   );
--- app/_component/mainContent/MainContent.tsx
+++ app/_component/mainContent/MainContent.tsx
@@ -12,30 +12,32 @@ import "./index.css";
 export const MainContent = () => {
   return (
     <>
-      <section className="section-about md:px-12 lg:px-20">
+      <section className="section-about">
         <HeadingSecondary
           content="Exciting tour for adventure people"
-          addClass="py-2 mb-14"
+          addClass="py-2 mb-12"
         />
-        <div className="flex max-w-[96rem] mx-auto py-4 md:gap-12 lg:gap-20 mt-28">
-          <aside className="w-full flex flex-col gap-4">
-            <Heading
-              addClass="mt-6"
-              content="You'r going to fall in love with nature"
-            />
-            <PText content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consequatur ad accusantium, exercitationem recusandae placeat itaque perferendis quisquam, quis, hic alias velit dolore provident facilis?" />
-            <Heading
-              addClass="mt-6"
-              content="Live adventures like you have never before"
-            />
-            <PText content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur ullam amet dolor assumenda suscipit provident enim rem sint ea nihil." />
-            <LinkButtonSimple
-              url={"/"}
-              btnName={`Learn more →`}
-              addClass="mt-4"
-            />
+        <div className="flex max-w-[96rem] max-xl:flex-col-reverse mx-auto py-4 md:gap-12 lg:gap-20 mt-8">
+          <aside className="w-full flex flex-col justify-center items-end ">
+            <div className="md:11/12 2xl:w-10/12  gap-4">
+              <Heading
+                addClass="mt-4"
+                content="You'r going to fall in love with nature"
+              />
+              <PText content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consequatur ad quis, hic alias velit dolore provident facilis?" />
+              <Heading
+                addClass="mt-6"
+                content="Live adventures like you have never before"
+              />
+              <PText content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur ullam amet dolor assumenda suscipit provident enim rem sint ea nihil." />
+              <LinkButtonSimple
+                url={"/"}
+                btnName={`Learn more →`}
+                addClass="mt-4"
+              />
+            </div>
           </aside>
-          <aside className="w-full md:pl-20">
+          <aside className="w-full md:pl-12 min-h-96">
             <ImageComposition />
           </aside>
         </div>
