fb13357af0358151d31dd95daa184efe3ec1e84d
om-dv <om.bharti@datavinci.services>
2024-02-04 20:59:38 +0530

    update data in cards

--- app/_component/card/CardWithBack.tsx
+++ app/_component/card/CardWithBack.tsx
@@ -1,21 +1,49 @@
+import { Heading } from "..";
 import "./index.css";
 
-export const CardWithBack = () => {
+interface Props {
+  src: string;
+  price: string;
+  peopleCount: string;
+  guidsCount: string;
+  difficulty: string;
+  highlight: string;
+  // peopleCount
+}
+
+export const CardWithBack = ({
+  src,
+  price,
+  peopleCount,
+  guidsCount,
+  difficulty,
+  highlight,
+}: Props) => {
   return (
     <div className="card-with-back">
       <div className="card-front">
         <div>
-          <img src="" />
+          <img src={src} width={"100%"} height={"12rem"} />
         </div>
         <div>
-          <h4>{"5 dollar"}</h4>
-          <h6>{"upto 20 people"}</h6>
-          <h6>{"2 tour guids"}</h6>
-          <h6>{"sleep in coosy hotel"}</h6>
-          <h6>{"Difficulty: very easy"}</h6>
+          {/* <h4 className="py-3 border-t-2 border-b-2">{price}</h4> */}
+          <Heading content={price} addClass="py-3 border-t-2 border-b-2" />
+          <Heading
+            content={peopleCount}
+            addClass="py-3 border-t-2 border-b-2"
+          />
+          <Heading content={price} addClass="py-3 border-t-2 border-b-2" />
+          {/* <h6>{"sleep in coosy hotel"}</h6> */}
+          <Heading content={highlight} addClass="py-3 border-t-2 border-b-2" />
+          {/* <h6>{guidsCount}</h6> */}
+          <Heading content={guidsCount} addClass="py-3 border-t-2 border-b-2" />
+          {/* <h6>{"Difficulty: very easy"}</h6> */}
+          <Heading content={difficulty} addClass="py-3 border-t-2 border-b-2" />
         </div>
       </div>
-      <div className="card-back"> back</div>
+      <div className="card-back">
+        <Heading content={price} />
+      </div>
     </div>
   );
 };
--- app/_component/cardsSection/CardsSection.tsx
+++ app/_component/cardsSection/CardsSection.tsx
@@ -9,9 +9,30 @@ export const CardsSection = () => {
         addClass="py-2 mb-12 mt-16 "
       />
       <div>
-        <CardWithBack />
-        <CardWithBack />
-        <CardWithBack />
+        <CardWithBack
+          src={"assets/image-1.jpg"}
+          price="$ 150"
+          peopleCount="upto 20 people"
+          guidsCount="2 tour guids"
+          difficulty="Difficulty: very easy"
+          highlight="sleep in coosy hotel"
+        />
+        <CardWithBack
+          src={"assets/image-2.jpg"}
+          price="$ 250"
+          peopleCount="upto 35 people"
+          guidsCount="3 tour guids"
+          difficulty="Difficulty: very medium"
+          highlight="sleep in coosy hotel"
+        />
+        <CardWithBack
+          src={"assets/image-3.jpg"}
+          price="$ 300"
+          highlight="sleep in coosy hotel"
+          peopleCount="upto 50 people"
+          guidsCount="4 tour guids"
+          difficulty="Difficulty: very hard"
+        />
       </div>
     </section>
   );
