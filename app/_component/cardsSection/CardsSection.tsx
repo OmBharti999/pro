0ea95821d2bdd49bdc42d15d36ebba8fc4694526
om-dv <om.bharti@datavinci.services>
2024-02-04 23:58:03 +0530

    update bg and border

--- app/_component/cardsSection/CardsSection.tsx
+++ app/_component/cardsSection/CardsSection.tsx
@@ -8,31 +8,33 @@ export const CardsSection = () => {
         content="Most popular tours"
         addClass="py-2 mb-12 mt-16 "
       />
-      <div className="md:flex">
-        <CardWithBack
-          src={"assets/image-1.jpg"}
-          price="$ 150"
-          peopleCount="upto 20 people"
-          guidsCount="2 tour guids"
-          difficulty="Difficulty: very easy"
-          highlight="sleep in coosy hotel"
-        />
-        <CardWithBack
-          src={"assets/image-2.jpg"}
-          price="$ 250"
-          peopleCount="upto 35 people"
-          guidsCount="3 tour guids"
-          difficulty="Difficulty: very medium"
-          highlight="sleep in coosy hotel"
-        />
-        <CardWithBack
-          src={"assets/image-3.jpg"}
-          price="$ 300"
-          highlight="sleep in coosy hotel"
-          peopleCount="upto 50 people"
-          guidsCount="4 tour guids"
-          difficulty="Difficulty: very hard"
-        />
+      <div className="max-w-7xl flex-center md:flex-row mx-auto">
+        <div className="flex justify-evenly w-full">
+          <CardWithBack
+            src={"assets/image-1.jpg"}
+            price="$ 150"
+            peopleCount="upto 20 people"
+            guidsCount="2 tour guids"
+            difficulty="Difficulty: very easy"
+            highlight="sleep in coosy hotel"
+          />
+          <CardWithBack
+            src={"assets/image-2.jpg"}
+            price="$ 250"
+            peopleCount="upto 35 people"
+            guidsCount="3 tour guids"
+            difficulty="Difficulty: very medium"
+            highlight="sleep in coosy hotel"
+          />
+          <CardWithBack
+            src={"assets/image-3.jpg"}
+            price="$ 300"
+            highlight="sleep in coosy hotel"
+            peopleCount="upto 50 people"
+            guidsCount="4 tour guids"
+            difficulty="Difficulty: very hard"
+          />
+        </div>
       </div>
     </section>
   );
