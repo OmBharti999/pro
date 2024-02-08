92c1857a61917579bbcbe49f2b32b59d2c01dea8
om-dv <om.bharti@datavinci.services>
2024-02-08 23:58:23 +0530

    add dynamic name and other field

--- app/_component/card/StoriesCard.tsx
+++ app/_component/card/StoriesCard.tsx
@@ -1,8 +1,18 @@
 import { Heading, PText } from "..";
 import "./index.css";
-interface Props {}
+interface Props {
+  cardHeading: string;
+  cardContent: string;
+  imgUrl: string;
+  imgName: string;
+}
 
-export const StoriesCard = ({}: Props) => {
+export const StoriesCard = ({
+  cardContent,
+  cardHeading,
+  imgName,
+  imgUrl,
+}: Props) => {
   return (
     <div className="stories-card shadow-big">
       <figure className="stories-shape"></figure>
--- app/_component/storiesSection/StoriesSection.tsx
+++ app/_component/storiesSection/StoriesSection.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { HeadingSecondary, StoriesCard } from "..";
 
+const heading1="I had the best week ever with my family"
+const heading2="I had the best week ever with my family"
+const content1="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores labore, quisquam obcaecati recusandae tempora similique libero asperiores molestiae sint aspernatur rem sequi, odit distinctio quae porro deserunt nihil at eos!"
+const content2="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores labore, quisquam obcaecati recusandae tempora similique libero asperiores molestiae sint aspernatur rem sequi, odit distinctio quae porro deserunt nihil at eos!"
 export const StoriesSection = () => {
   return (
     <section>
@@ -10,8 +14,8 @@ export const StoriesSection = () => {
       />
 
       <div className="flex-center flex-col gap-12">
-        <StoriesCard />
-        <StoriesCard />
+        <StoriesCard cardHeading={heading1} cardContent={content1} imgUrl={""} imgName={"JAn pre"}/>
+        <StoriesCard cardHeading={heading2} cardContent={content2} imgUrl={""} imgName={"Aug gun"}/>
       </div>
     </section>
   );
