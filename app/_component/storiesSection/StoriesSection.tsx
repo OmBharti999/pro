f03947451c4f9a6ff0f4676c59240d2db01b0a28
om-dv <om.bharti@datavinci.services>
2024-02-09 23:28:40 +0530

    update text

--- app/_component/storiesSection/StoriesSection.tsx
+++ app/_component/storiesSection/StoriesSection.tsx
@@ -4,11 +4,11 @@ import { HeadingSecondary, StoriesCard } from "..";
 // import Girl from "@/public/assets/girl.jpg";
 
 const heading1 = "I had the best week ever with my family";
-const heading2 = "I had the best week ever with my family";
+const heading2 = "WOW! My life is completely different now";
 const content1 =
-  "Last week was an absolute blast with my family, filled with laughter and unforgettable memories. From exploring new places together to sharing delicious meals, every moment was special. We bonded over shared experiences and cherished quality time spent with each other. Whether it was a cozy night in or an adventure outdoors, the joy of being surrounded by loved ones made it the best week ever. As we reminisce about our adventures, I am grateful for the precious moments we shared and eagerly look forward to making more memories together in the future.";
+  "Last week was an absolute blast with my family, filled with laughter and unforgettable memories. From exploring new places together to sharing delicious meals, every moment was special. We bonded over shared experiences and cherished quality time spent with each other. As we reminisce about our adventures, I am grateful for the precious moments we shared and eagerly look forward to making more memories together in the future.";
 const content2 =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores labore, quisquam obcaecati recusandae tempora similique libero asperiores molestiae sint aspernatur rem sequi, odit distinctio quae porro deserunt nihil at eos!";
+  "My life has undergone a remarkable transformation, and every day feels like a new adventure waiting to unfold. From embracing new opportunities to rediscovering passions, the journey has been exhilarating. I navigate through challenges with confidence and resilience. As I reflect on this newfound chapter, I am grateful for the growth and positive changes that have enriched my life beyond measure.";
 export const StoriesSection = () => {
   return (
     <section>
@@ -19,16 +19,16 @@ export const StoriesSection = () => {
 
       <div className="flex-center flex-col gap-12">
         <StoriesCard
-          cardHeading={heading1}
-          cardContent={content1}
-          imgUrl={"/assets/boy.jpg"}
-          imgName={"Tom Hile"}
-        />
-        <StoriesCard
           cardHeading={heading2}
           cardContent={content2}
           imgUrl={"/assets/girl.jpg"}
-          imgName={"Aug Jusee"}
+          imgName={"Marry Smith"}
+        />
+        <StoriesCard
+          cardHeading={heading1}
+          cardContent={content1}
+          imgUrl={"/assets/boy.jpg"}
+          imgName={"Jack Wilson"}
         />
       </div>
     </section>
