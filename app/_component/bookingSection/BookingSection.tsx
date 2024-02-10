79612b7a8d55ac56258ab376d813bdcf08303625
om-dv <om.bharti@datavinci.services>
2024-02-10 23:06:26 +0530

    add book form section css

--- app/_component/bookingSection/BookingSection.tsx
+++ app/_component/bookingSection/BookingSection.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./index.css"
 export const BookingSection = () => {
   return (
     <section className="p-14 flex-center bg-gradient-to-r from-green-500 to-green-700 h-screen">
-      <div className="max-w-3xl w-full bg-white h-64"></div>
+      <div className="max-w-3xl w-full h-64 book"></div>
     </section>
   );
 };
