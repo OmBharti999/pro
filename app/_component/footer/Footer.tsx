89ee4d30fa8524d0d8ecece9f908954663f51095
om-dv <om.bharti@datavinci.services>
2024-02-13 00:03:11 +0530

    add footer bg and text color

--- app/_component/footer/Footer.tsx
+++ app/_component/footer/Footer.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-neutral-900 text-white">
+      <div className="footer-logo">{/* image */}logo</div>
+      <div className="row">
+        <div className="part">
+          <ul>
+            <li>
+              <Link href={""}>company</Link>
+            </li>
+            <li>
+              <Link href={""}>contact us</Link>
+            </li>
+            <li>
+              <Link href={""}>careers</Link>
+            </li>
+            <li>
+              <Link href={""}>privacy</Link>
+            </li>
+            <li>
+              <Link href={""}>terms</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="part">
+          <div className="footer-copyright-text"></div>
+          <p>
+            Build by
+            <Link href={""}>Om</Link> for fun and learning.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
