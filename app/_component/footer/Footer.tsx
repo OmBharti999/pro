0f1df3c765b36cb9b85c522b72324974abc2eee6
om-dv <om.bharti@datavinci.services>
2024-02-13 23:25:07 +0530

    fix more content

--- app/_component/footer/Footer.tsx
+++ app/_component/footer/Footer.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import "./index.css";
 export const Footer = () => {
   return (
-    <footer className="bg-neutral-900 text-white min-h-[35rem]">
+    <footer className="bg-neutral-900 text-slate-100 min-h-[35rem] text-sm">
       <div className="footer-logo flex-center min-h-80">{/* image */}logo</div>
-      <div className="row flex justify-between gap-x-20 max-w-3xl mx-auto">
-        <div className="part ">
-          <ul className="flex gap-x-4  uppercase text-xs">
+      <div className="row flex justify-between gap-x-20 max-w-4xl mx-auto">
+        <div className="part">
+          <ul className="flex gap-x-4  uppercase">
             <li>
               <Link href={""}>company</Link>
             </li>
@@ -24,16 +24,16 @@ export const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="part">
+        <div className="part max-w-sm">
           <div className="footer-copyright-text"></div>
           <p className="text-gray-400">
             Build by{" "}
             <Link className="text-white/90" href={""}>
               Om
             </Link>{" "}
-            for fun and learning. Copyright © by Om, you are absolutely free to use
-            this in your production or self project just dont forget to mention
-            me.
+            for fun and learning. Copyright © by Om, you are absolutely free to
+            use this in your production or self project just dont forget to
+            mention me.
           </p>
         </div>
       </div>
