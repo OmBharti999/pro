9b37d9b08252487b3a21896e99753fbc7760a22f
om-dv <om.bharti@datavinci.services>
2024-02-13 23:04:02 +0530

    fix booking section bug

--- app/_component/bookingSection/BookingSection.tsx
+++ app/_component/bookingSection/BookingSection.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 
 export const BookingSection = () => {
   return (
-    <section className="p-14 flex-center bg-gradient-to-r from-green-500 to-green-700 h-screen">
+    <section className="p-14 flex-center bg-gradient-to-r from-green-500 to-green-700 py-28">
       <div className="max-w-6xl w-full book">
         <BookForm />
       </div>
