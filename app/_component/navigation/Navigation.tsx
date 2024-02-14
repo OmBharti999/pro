27711f80b2486734738138f9798f6d105964f268
om-dv <om.bharti@datavinci.services>
2024-02-14 23:31:45 +0530

    align button and bg at right top

--- app/_component/navigation/Navigation.tsx
+++ app/_component/navigation/Navigation.tsx
@@ -3,10 +3,11 @@ import "./index.css";
 export const Navigation = () => {
   return (
     <div className="fixed right-5 z-50">
-      <div>
+      <div className="nav-button flex-center">
         <input type="checkbox" id="nav-input" />
         <label htmlFor="nav-input">Menu</label>
       </div>
+      <div className="nav-bg-circular"></div>
       <nav className="nav-expand flex-center ">
         <ol>
           <li className="nav-item">About Natous</li>
