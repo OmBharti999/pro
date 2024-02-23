c90e741c85ecd3ebed9b32a5d120cbf2e8075c27
om-dv <om.bharti@datavinci.services>
2024-02-23 23:25:59 +0530

    TODO: try to do this with css

--- app/_component/navigation/Navigation.tsx
+++ app/_component/navigation/Navigation.tsx
@@ -1,4 +1,7 @@
+// import { CgMenuLeft } from "react-icons/cg";
+// import { IoClose } from "react-icons/io5";
 import { IconSwitcher } from "./_component/IconSwitcher";
+
 import "./index.css";
 
 export const Navigation = () => {
@@ -7,6 +10,9 @@ export const Navigation = () => {
       <input type="checkbox" id="nav-input" />
       <label htmlFor="nav-input" className="nav-button flex-center">
         <IconSwitcher />
+        {/* <div className="nav-icon text-2xl">
+          <CgMenuLeft className="nav-icon-menu"/> <IoClose className="nav-icon-close"/>
+        </div> */}
       </label>
       <div className="nav-bg-circular"></div>
       <nav className="nav-expand flex-center ">
