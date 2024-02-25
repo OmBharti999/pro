0da8dbc7e3901b68dffe14e0e7cfe8e1ea2419bf
om-dv <om.bharti@datavinci.services>
2024-02-25 23:22:38 +0530

    update content for popup

--- app/_component/popup/Popup.tsx
+++ app/_component/popup/Popup.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Heading, HeadingSecondary } from "..";
+import "./index.css";
 
 export const Popup = () => {
   return (
-    <div>Popup</div>
-  )
-}
+    <div className="popup flex-center">
+      <div className="popup_content">
+        <aside className="popup_content_left">{/* <img */}</aside>
+        <section className="popup_content_right">
+          <HeadingSecondary content="Start Booking Now" addClass="py-2 mb-12 text-left" />
+         
+          <h3 className="text-gray-700 font-bold text-left">
+            IMPORTANT - PLEASE READ THE TERMS AND CONDITIONS FIRST
+          </h3>
+        </section>
+      </div>
+    </div>
+  );
+};
